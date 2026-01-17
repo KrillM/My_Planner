bbddd5c997ed762616eff34cc796b0f575e2b184
krille <mks160615@gmail.com>
2026-01-17 11:02:36 +0900

    alter table crew add column alarm

--- server/model/crew.js
+++ server/model/crew.js
@@ -8,6 +8,7 @@ module.exports = (sequelize) => {
     password: { type: DataTypes.STRING(600) },
     motto: { type: DataTypes.STRING(200) },
     profileImage: { type: DataTypes.STRING(600) },
+    alarm: { type: DataTypes.DATE },
     loginType: { type: DataTypes.STRING(10), defaultValue: 'MY_PLANNER', allowNull: false },
     creationTime: { type: DataTypes.DATE, allowNull: false },
     modifyTime: { type: DataTypes.DATE, allowNull: false }
