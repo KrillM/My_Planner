f4f12d7b09dc8a5a71a7c32c3a96d58a7776433c
krille <mks160615@gmail.com>
2026-02-22 16:50:46 +0900

    Fix: add column

--- server/model/frequency.js
+++ server/model/frequency.js
@@ -5,6 +5,7 @@ module.exports = (sequelize) => {
     frequencyId: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true },
     crewId: { type: DataTypes.STRING(40), primaryKey: true },
     title: { type: DataTypes.STRING(200), allowNull: false },
+    frequencyCount: {type: DataTypes.INTEGER, allowNull: false, defaultValue: 0},
     creationTime: { type: DataTypes.DATE, allowNull: false },
     modifyTime: { type: DataTypes.DATE, allowNull: false }
   }, { tableName: 'frequency', timestamps: false });
