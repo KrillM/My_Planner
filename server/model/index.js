f1a536c4935d4b9b776b9a238135ace8aacd446e
krille <mks160615@gmail.com>
2026-02-22 14:39:49 +0900

    Fix: index

--- server/model/index.js
+++ server/model/index.js
@@ -20,7 +20,7 @@ db.MonthlyPlan = require('./monthlyPlan')(sequelize);
 db.Goal = require('./goal')(sequelize);
 db.Date = require('./date')(sequelize);
 db.ToDo = require('./toDo')(sequelize);
-db.TodoMemo = require('./todoMemo')(sequelize);
+db.FrequencyMemo = require('./frequencyMemo')(sequelize);
 db.DateMemo = require('./dateMemo')(sequelize);
 db.Frequency = require('./frequency')(sequelize);
 db.List = require('./list')(sequelize);
@@ -46,11 +46,9 @@ db.YearlyPlan.hasMany(db.MonthlyPlan, {
 db.Date.hasMany(db.ToDo, { foreignKey: 'dateId', ...cascadeOptions });
 db.Date.hasMany(db.DateMemo, { foreignKey: 'dateId', ...cascadeOptions });
 
-// ToDo -> TodoMemo
-db.ToDo.hasMany(db.TodoMemo, { foreignKey: 'toDoId', ...cascadeOptions });
-
-// Frequency -> List
+// Frequency 기반 관계
 db.Frequency.hasMany(db.List, { foreignKey: 'frequencyId', ...cascadeOptions });
+db.Frequency.hasMany(db.FrequencyMemo, { foreignKey: 'frequencyId', ...cascadeOptions });
 
 // belongsTo 설정 (역방향)
 db.YearlyPlan.belongsTo(db.Crew, { foreignKey: 'crewId' });
@@ -58,9 +56,9 @@ db.MonthlyPlan.belongsTo(db.YearlyPlan, { foreignKey: 'yearlyPlanId' });
 db.Goal.belongsTo(db.Crew, { foreignKey: 'crewId' });
 db.Date.belongsTo(db.Crew, { foreignKey: 'crewId' });
 db.ToDo.belongsTo(db.Date, { foreignKey: 'dateId' });
-db.TodoMemo.belongsTo(db.ToDo, { foreignKey: 'toDoId' });
 db.DateMemo.belongsTo(db.Date, { foreignKey: 'dateId' });
 db.Frequency.belongsTo(db.Crew, { foreignKey: 'crewId' });
+db.FrequencyMemo.belongsTo(db.ToDo, { foreignKey: 'frequencyId' });
 db.List.belongsTo(db.Frequency, { foreignKey: 'frequencyId' });
 db.Event.belongsTo(db.Crew, { foreignKey: 'crewId' });
 
